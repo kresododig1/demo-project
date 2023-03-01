ec43b70a75990a386d3796964cf7d0b4d049cd2c
Krešimir Dodig <kresododig@yahoo.com>
2023-03-01 23:27:42 +0100

    Changes in driver class

--- src/test/java/cbt/Driver.java
+++ src/test/java/cbt/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "3.85.177.147";
+                        String gridAddress = "100.25.193.96";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
