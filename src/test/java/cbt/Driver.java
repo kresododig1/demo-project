2983280b2ebbc924ddaf6c78b092a26f906a0348
Krešimir Dodig <kresododig@yahoo.com>
2023-03-07 11:51:02 +0100

    Changes in implicit wait in step_def and adding tag in cukesRunner

--- src/test/java/cbt/Driver.java
+++ src/test/java/cbt/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "100.25.193.96";
+                        String gridAddress = "54.172.29.98";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
--- src/test/java/cbt/StepDefs.java
+++ src/test/java/cbt/StepDefs.java
@@ -21,7 +21,7 @@ public class StepDefs {
 
     @Given("^I am on the home page$")
     public void i_am_on_the_home_page() throws Throwable {
-        Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        Driver.getDriver().manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get("http://etsy.com");
 
